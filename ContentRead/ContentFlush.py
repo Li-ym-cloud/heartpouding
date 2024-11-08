f586370a5d0b4f0b40a451e4c4a2cdfc9bd714f9
Li-ym-cloud <2308570708@qq.com>
2024-11-08 20:07:58 +0800

    实现超出字体能够在范围内实现滚动

--- ContentRead/ContentFlush.py
+++ ContentRead/ContentFlush.py
@@ -7,17 +7,26 @@
 @创建日期 2024/11/6
 @描述 文本页面操作类
 """
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 import PGDBReadWrite as pgrw
 
 app = Flask(__name__)
 
-
+# 首页显示文本
 @app.route('/')
 def content_show():
-    record = pgrw.get_read_context("select content from public.content_flush_item order by random() limit 1;")
+    record = pgrw.get_read_context(
+        "select content from public.content_flush_item where content is not null order by random() limit 1;"
+    )
     return render_template('ContentFlush.html', record=record[0])
 
+# 获取新的随机文本
+@app.route('/refresh_content')
+def refresh_content():
+    record = pgrw.get_read_context(
+        "select content from public.content_flush_item where content is not null order by random() limit 1;"
+    )
+    return jsonify({"record": record[0]})
 
 if __name__ == '__main__':
     app.run()
